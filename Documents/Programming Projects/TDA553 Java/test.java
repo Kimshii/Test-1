7245afcd7d00a7f7dae23bb777e61a12be377e9d
JacobAndersso <jacan@student.chalmers.se>
2022-11-07 14:55:31 +0100

    no

--- Documents/Programming Projects/TDA553 Java/test.java
+++ Documents/Programming Projects/TDA553 Java/test.java
@@ -1,7 +1,8 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("ey");
-
-
+        if ( 1 == 2) {
+            System.out.println("no");
+        }
     }
 }
